a76364e609e299bcdb2ca75a862321789ef184ec
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-06-12 13:57:52 +0200

    Oppsett av prosjektet etter mønster fra designsystemet

---
+++ src/development/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Tidslinje from '../../package/src/Tidslinje';
+
+const App = () => (
+  <div>
+    <Tidslinje />
+  </div>
+);
+
+export default App;
---
+++ src/package/src/Tidslinje.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tidslinje = () => (
+  <div />
+);
+
+export default Tidslinje;
