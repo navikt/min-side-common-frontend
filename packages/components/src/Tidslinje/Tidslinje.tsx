65ceefafa74d162a576f25c564ae151978540046
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-10-30 14:42:17 +0100

    Første versjon av tidslinjen

--- packages/components/src/Tidslinje/Tidslinje.tsx
+++ packages/components/src/Tidslinje/Tidslinje.tsx
@@ -1,9 +1,85 @@
 import React, { ReactElement } from 'react';
 import Panel from 'nav-frontend-paneler';
+import { Normaltekst } from 'nav-frontend-typografi';
+import Advarsel from './Advarsel';
+import Informasjon from './Informasjon';
+import Ferdig from './Ferdig';
 
-const Tidslinje = (): ReactElement => (
+interface Props {
+  notifikasjon: any;
+}
+
+export interface TidslinjeProps {
+  notifikasjoner: any;
+  forventninger?: string[];
+}
+
+export interface SekvensProps {
+  tekst: string;
+}
+
+const tidslinjeIkon = (type: string) => {
+  switch (type) {
+    case 'Beskjed':
+      return <Informasjon />;
+    case 'Oppgave':
+      return <Advarsel />;
+    case 'Statusoppdatering':
+      return <Ferdig />;
+    default:
+      return null;
+  }
+};
+
+const tidslinjeText = (notifikasjon: any) => {
+  switch (notifikasjon.statusGlobal) {
+    case 'SENDT':
+      return 'sendt';
+    case 'MOTTATT':
+      return 'Status i saken din: Mottatt';
+    case 'UNDER_BEHANDLING':
+      return 'under behandling';
+    case 'FERDIG':
+      return 'ferdig';
+    default:
+      return notifikasjon.tekst;
+  }
+};
+
+const SekvensMedIkon = ({ notifikasjon }: Props): ReactElement => (
+  <div className="sekvens-med-ikon">
+    <div className="sekvens-med-ikon__ikon">
+      {tidslinjeIkon(notifikasjon.type)}
+    </div>
+    <div className="sekvens-med-ikon__tekst">
+      <Normaltekst>
+        {tidslinjeText(notifikasjon)}
+      </Normaltekst>
+    </div>
+  </div>
+);
+
+const Sekvens = ({ tekst }: SekvensProps) => (
+  <div className="sekvens-uten-ikon">
+    <div className="sekvens-uten-ikon__tekst">
+      <Normaltekst>
+        {tekst}
+      </Normaltekst>
+    </div>
+  </div>
+);
+
+
+const Tidslinje = ({ notifikasjoner, forventninger }: TidslinjeProps): ReactElement => (
   <Panel border>
-      Tidslinje
+    <div className="tidslinje-container">
+      {forventninger && forventninger.map((tekst) => (
+        <Sekvens key={tekst} tekst={tekst} />
+      ))}
+      {notifikasjoner.map((notifikasjon: any) => (
+        <SekvensMedIkon notifikasjon={notifikasjon} />
+      ))}
+    </div>
   </Panel>
 );
 
